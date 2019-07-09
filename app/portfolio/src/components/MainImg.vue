81adfe382e6f8dc6acf68f7358d73a917c896af1
azul915 <k.aoyagi940516@gmail.com>
2019-07-10 01:37:25 +0900

    VueTyperおよびCSS調整

--- app/portfolio/src/components/MainImg.vue
+++ app/portfolio/src/components/MainImg.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="mainImg">
-        <link href='https://fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
-        <div class="container">
-        <div class="overlay">
-            <h1><Greeting/></h1>
+
+      <div class="body">
+        <div id="gradient">
+          <Greeting/>
         </div>
-        </div>
+      </div>
+
     </div>
+
 </template>
 
 <script>
+import '../../public/js/gradation.js'
 import Greeting from '@/components/Greeting.vue'
 export default { 
     name: 'MainImg',
@@ -18,46 +21,3 @@ export default {
     }
 }
 </script>
-
-<style lang="scss">
-html, body, .container {
-  width: 100%;
-  height: 100%;
-}
-
-.container,
-.overlay:before {
-  background: url("../../public/images/CLOSING_THE_DISTANCE.png") no-repeat fixed 0 0 / cover;
-}
-
-
-.container {
-  align-items: center;
-  display: flex;
-  justify-content: center;
-}
-
-.overlay {
-  max-height: 200px;
-  margin: 0 auto;
-  max-width: 768px;
-  overflow: hidden;
-  padding: 50px;
-  position: relative;
-  color: white;
-  font-family: 'Lato';
-  position: relative;
-  text-align: center;
-  z-index: 0;
-  &:before {
-    content: '';
-    filter: blur(20px);
-    height: 100%;
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 100%;
-    z-index: -1;
-  }
-}
-</style>
