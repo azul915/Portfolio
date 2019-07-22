35932ffd20329586e1e28c678dfebee2abaa7cfb
azul915 <k.aoyagi940516@gmail.com>
2019-07-23 08:42:22 +0900

    マウスオーバー、マウスリーブでボックスの開閉を行う

--- app/portfolio/src/components/Skills.vue
+++ app/portfolio/src/components/Skills.vue
@@ -4,13 +4,14 @@
 
         <div class="inner">
 
-            <div class="grid">
-                <div id="server_side" class="grid_title">サーバーサイド</div>
+            <div id="server_side" class="grid">
+                <div class="grid_title">サーバーサイド</div>
                 <ul class="container">
-                    <li v-bind:class="{ skill_collection_box: isNormal, skill_collection_box_anim: isHovered }"
-                        v-on:mouseover="showDetail" v-on:mouseleave="showSimple"
-                        v-for="(skill, id) in server_side" :key="id">
-
+                    <li class="skill_collection_box"
+                        v-bind:class="{ selected : activeSkill === skill }"
+                        v-on:mouseover="activeSkill = skill"
+                        v-on:mouseleave="activeSkill = ''"
+                        v-for="skill in server_side" :key="skill.id">
                         <div class="inner_collection_box">
                             <div class="box_self_evaluation">
                                 <PieChart class="chart" v-bind:self-eval="skill.self_evaluation" />
@@ -25,12 +26,10 @@
                                     <div class="duration"><li>経験：{{ skill.duration }}ヶ月程度</li></div>
                                 </ul>
                             </div>
+                            <div class="skillDetail clearFix">
+                                <div class="detail">{{ skill.detail }}</div>
+                            </div>
                         </div>
-                        <!-- isHoveredで表示▼ -->
-                        <div class="skillDetail clearFix">
-                            <div class="detail"></div>
-                        </div>
-                        <!-- isHoveredで表示▲-->
 
                     </li>
                 </ul>
@@ -39,9 +38,11 @@
             <div id="front_end" class="grid">
                 <div class="grid_title">フロントエンド</div>
                 <ul class="container">
-                    <li v-bind:class="{ skill_collection_box: isNormal, skill_collection_box_anim: isHovered }"
-                        v-on:mouseover="showDetail" v-on:mouseleave="showSimple"
-                        v-for="(skill, id) in front_end" :key="id">
+                    <li class="skill_collection_box"
+                        v-bind:class="{ selected : activeSkill === skill }"
+                        v-on:mouseover="activeSkill = skill"
+                        v-on:mouseleave="activeSkill = ''"
+                        v-for="skill in front_end" :key="skill.id">
 
                         <div class="inner_collection_box">
                             <div class="box_self_evaluation">
@@ -57,12 +58,10 @@
                                     <div class="duration"><li>経験：{{ skill.duration }}ヶ月程度</li></div>
                                 </ul>
                             </div>
+                            <div class="skillDetail clearFix">
+                                <div class="detail">{{ skill.detail }}</div>
+                            </div>
                         </div>
-                        <!-- isHoveredで表示▼ -->
-                        <div class="skillDetail clearFix">
-                            <div class="detail"></div>
-                        </div>
-                        <!-- isHoveredで表示▲-->
 
                     </li>
                 </ul>
@@ -71,9 +70,11 @@
              <div id="infrastructure" class="grid">
                 <div class="grid_title">フロントエンド</div>
                 <ul class="container">
-                    <li v-bind:class="{ skill_collection_box: isNormal, skill_collection_box_anim: isHovered }"
-                        v-on:mouseover="showDetail" v-on:mouseleave="showSimple"
-                        v-for="(skill, id) in infrastructure" :key="id">
+                    <li class="skill_collection_box"
+                        v-bind:class="{ selected : activeSkill == skill }"
+                        v-on:mouseover="activeSkill = skill"
+                        v-on:mouseleave="activeSkill = ''"
+                        v-for="skill in infrastructure" :key="skill.id">
 
                         <div class="inner_collection_box">
                             <div class="box_self_evaluation">
@@ -89,12 +90,10 @@
                                     <div class="duration"><li>経験：{{ skill.duration }}ヶ月程度</li></div>
                                 </ul>
                             </div>
+                            <div class="skillDetail clearFix">
+                                <div class="detail">{{ skill.detail }}</div>
+                            </div>
                         </div>
-                        <!-- isHoveredで表示▼ -->
-                        <div class="skillDetail clearFix">
-                            <div class="detail"></div>
-                        </div>
-                        <!-- isHoveredで表示▲-->
 
                     </li>
                 </ul>
@@ -114,23 +113,12 @@ export default {
             server_side: [],
             front_end: [],
             infrastructure: [],
-            isNormal: true,
-            isHovered: false,
+            activeSkill: "",
         }
     },
 
     methods: {
-        // マウスオーバーしたときの挙動
-        showDetail() {
-            //
-
-        },
-
-        // マウスリーブしたときの挙動
-        showSimple() {
-            //
-
-        },
+        
     },
 
     // インスタンスライフサイクルフック
